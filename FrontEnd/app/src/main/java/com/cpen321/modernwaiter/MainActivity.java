433f3da8d50f42fcc1752bc14e5695a0ddc28452
dahiya18 <harmeetadahiya@gmail.com>
2020-10-18 18:45:04 -0700

    added design for payment fragment

--- FrontEnd/app/src/main/java/com/cpen321/modernwaiter/MainActivity.java
+++ FrontEnd/app/src/main/java/com/cpen321/modernwaiter/MainActivity.java
@@ -12,11 +12,12 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
--- FrontEnd/app/src/main/java/com/cpen321/modernwaiter/ui/pay/PayFragment.java
+++ FrontEnd/app/src/main/java/com/cpen321/modernwaiter/ui/pay/PayFragment.java
@@ -1,10 +1,16 @@
 package com.cpen321.modernwaiter.ui.pay;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +22,10 @@ import com.cpen321.modernwaiter.R;
 
 public class PayFragment extends Fragment {
 
+    private Button pay_for_all;
+    private Button split_evenly;
+    private Button pay_per_item;
+    final static String TAG = "PayFragment";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +36,36 @@ public class PayFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View root = inflater.inflate(R.layout.fragment_pay, container, false);
-        final TextView textView = root.findViewById(R.id.text_pay);
-
-        textView.setText("Pay Fragment");
+        pay_for_all = root.findViewById(R.id.pay_for_all);
+        pay_for_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Trying to pay for all");
+                Toast toast = Toast.makeText(getActivity(),"Trying to pay for everyone", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 190);
+                toast.show();
+            }
+        });
+        split_evenly = root.findViewById(R.id.split_evenly);
+        split_evenly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Trying to split evenly");
+                Toast toast = Toast.makeText(getActivity(),"Trying to split evenly and pay", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 190);
+                toast.show();
+            }
+        });
+        pay_per_item = root.findViewById(R.id.pay_per_item);
+        pay_per_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Trying to pay per item");
+                Toast toast = Toast.makeText(getActivity(),"Trying to pay per item", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 190);
+                toast.show();
+            }
+        });
 
         return root;
     }
