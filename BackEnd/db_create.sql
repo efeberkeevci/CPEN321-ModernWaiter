a4801bb5098307b86366bb82b27863d0cfd13a55
Efe Evci <efeberkeevci@gmail.com>
2020-10-20 14:25:10 +0300

    Placed Tawsif's db creation script in db_create.sql. Started implementing backend interface functions

---
+++ BackEnd/db_create.sql
@@ -0,0 +1,124 @@
+/* Database Initialization Script: */
+USE MODERN_WAITER_DB;
+
+create table options
+(
+    id   int          not null,
+    name varchar(250) not null,
+    cost double       not null,
+    constraint options_id_uindex
+        unique (id)
+);
+
+alter table options
+    add primary key (id);
+
+create table restaurant
+(
+    id                     int          not null,
+    name                   varchar(250) not null,
+    location               varchar(250) not null,
+    tax_percentage         double       not null,
+    service_fee_percentage double       not null,
+    constraint restaurant_id_uindex
+        unique (id)
+);
+
+alter table restaurant
+    add primary key (id);
+
+create table items
+(
+    id               int           not null,
+    restaurant_id    int           not null,
+    name             varchar(250)  not null,
+    type             varchar(250)  not null,
+    cost             double        not null,
+    description      varchar(1000) not null,
+    calories         double        not null,
+    popularity_count int           not null,
+    image            varchar(1000) not null,
+    constraint items_id_uindex
+        unique (id),
+    constraint items_restaurant_id_fk
+        foreign key (restaurant_id) references restaurant (id)
+);
+
+alter table items
+    add primary key (id);
+
+create table items_options
+(
+    items_id   int not null,
+    options_id int not null,
+    constraint items_options_items_id_fk
+        foreign key (items_id) references items (id),
+    constraint items_options_options_id_fk
+        foreign key (options_id) references options (id)
+);
+
+create table tables
+(
+    id           int not null,
+    table_number int not null,
+    constraint tables_id_uindex
+        unique (id)
+);
+
+alter table tables
+    add primary key (id);
+
+create table users
+(
+    id         int          not null,
+    username   varchar(250) not null,
+    email      varchar(250) not null,
+    created_at datetime     not null,
+    constraint users_email_uindex
+        unique (email),
+    constraint users_id_uindex
+        unique (id),
+    constraint users_username_uindex
+        unique (username)
+);
+
+alter table users
+    add primary key (id);
+
+create table orders
+(
+    id                int                  not null,
+    tables_id         int                  not null,
+    users_id          int                  not null,
+    restaurant_id     int                  not null,
+    amount            double               not null,
+    ordered_at        datetime             not null,
+    has_paid          tinyint(1) default 0 not null,
+    is_active_session tinyint(1) default 0 not null,
+    constraint orders_id_uindex
+        unique (id),
+    constraint orders_tables_id_fk
+        foreign key (tables_id) references tables (id),
+    constraint orders_users_id_fk
+        foreign key (users_id) references users (id)
+);
+
+alter table orders
+    add primary key (id);
+
+create table ordered_items
+(
+    id        int not null,
+    orders_id int not null,
+    items_id  int not null,
+    has_paid          tinyint(1) default 0 not null,
+    is_selected          tinyint(1) default 0 not null,
+    constraint ordered_items_id_uindex
+        unique (id),
+    constraint ordered_items_orders_id_fk
+        foreign key (orders_id) references orders (id)
+);
+
+alter table ordered_items
+    add primary key (id);
+
---
+++ BackEnd/interface_functions.js
@@ -0,0 +1,247 @@
+var mysql = require('mysql');
+
+/*
+Interfaces:
+Backend interface for Modern Waiter:
+
+Given, from QR code we get the restaurant id and table id
+
+The following are User API.
+*/
+
+//RESTAURANT
+
+/*
+Response:
+{
+    "id": 4,
+    "name" : "Jack's diner",
+    "location" : "East Vancouver",
+    "tax_percentage" : 12.0,
+    "service_fee_percentage" : 6.5
+} 
+*/
+function GetRestaurantById(id){
+    let sql_query = mysql.format("SELECT * FROM restaurant WHERE restaurant.id = ?", [id]);
+    con.query(sql_query, [id], function(err,result,fields){
+        if (err) throw err;
+        console.log(result);
+    });
+}
+
+
+//ITEMS
+
+/*
+Response:
+[{
+    "id" : 2,
+    "restaurant_id" : 4,
+    "name" : "Salad",
+    "description" : "blah blah",
+    "type" : "Sides",
+    "cost" : 5.50,
+    "calories" : 200,
+    "popularity_count" : 11,
+    "image" : "some url or local file path of image in vm"
+}, 
+{
+    "id" : 3,
+    "restaurant_id" : 4,
+    "name" : "Cheeseburger",
+    "description" : "blah blah",
+    "type" : "Burger",
+    "cost" : 8.50,
+    "calories" : 1100,
+    "popularity_count" : 19,
+    "image" : "some url or local file path of image in vm"
+}]
+*/
+/*
+function GetItemsByRestaurantId(restaurantId){
+
+}
+
+
+Items-Options:
+
+JSON GetOptionsIdsByItemsId(int itemsId);
+
+Response:
+
+[{
+    "id" : 1,
+    "items_id" : 3,
+    "options_id" : 1 
+},
+{
+    "id" : 2,
+    "items_id" : 3,
+    "options_id" : 2 
+},
+{
+    "id" : 3,
+    "items_id" : 3,
+    "options_id" : 3 
+}]
+
+Options: 
+
+JSON GetOptionsById(int id);
+
+[{
+    "id" : 1,
+    "name" : "Tomatoes",
+    "cost" : 0 
+},
+{
+    "id" : 2,
+    "name" : "Cherries",
+    "cost" : 0 
+},
+{
+    "id" : 3,
+    "name" : "Avocado",
+    "cost" : 1.50 
+}]
+
+Tables:
+
+JSON GetTableById(int id);
+
+Response:
+
+{ "table_number" : 13 }
+
+Users:
+
+JSON GetUserById(int id);
+
+Response:
+{
+    "id" : 1,
+    "username" : "tawsifh",
+    "email" : "tawsif@h.com",
+    "created_at" : "11Oct20 blah blah"
+}
+
+Orders:
+
+bool PostOrder(JSON requestBody);
+
+Request:
+
+[{
+    "id" : 1,
+    “user_id” : 2
+    "tables_id" : 3,
+    "restaurant_id" : 1,
+    "amount" : 25.0,
+    "ordered_at" : "some timestamp",
+    "has_paid" : false,
+    "is_active_session" : true
+}]
+
+bool UpdateOrderAmountById(int id, double amount);
+bool UpdateOrderHasPaidFlag(bool hasPaid);
+bool UpdateOrderIsActiveSessionFlag(bool isActiveSession);
+
+JSON GetOrdersByUserId(int userId, bool isActive);
+
+Response:
+
+[{
+    "id" : 1,
+    “users_id” : 2
+    "tables_id" : 3,
+    "restaurant_id" : 1,
+    "amount" : 25.0,
+    "ordered_at" : "some timestamp",
+    "has_paid" : false,
+    "is_active_session" : true
+}]
+
+JSON GetOrdersByTableId(int tableId, bool isActive);
+
+Response:
+
+[{
+    "id" : 1,
+    "users_id" : 2
+    "tables_id" : 3,
+    "restaurant_id" : 1,
+    "amount" : 25.0,
+    "ordered_at" : "some timestamp",
+    "has_paid" : false,
+    "is_active_session" : true
+},
+{
+    "id" : 5,
+    "users_id" : 4
+    "tables_id" : 3,
+    "restaurant_id" : 1,
+    "amount" : 35.0,
+    "ordered_at" : "some timestamp",
+    "has_paid" : false,
+    "is_active_session" : true
+},
+{
+    "id" : 11,
+    "users_id" : 5
+    "tables_id" : 3,
+    "restaurant_id" : 1,
+    "amount" : 10.0,
+    "ordered_at" : "some timestamp",
+    "has_paid" : false,
+    "is_active_session" : true
+}]
+
+Ordered Items:
+
+JSON GetOrderedItemsByOrderId(int orderId);
+
+Response:
+
+[{
+    "id" : 5,
+    "orders_id" : 11
+    "items_id" : 2,
+    "has_paid" : true,
+    “Is_selected” : false
+},
+{
+    "id" : 6,
+    "orders_id" : 11
+    "items_id" : 3,
+    "has_paid" : false,
+    “Is_selected: : false
+}]
+
+bool UpdateOrderItemsByOrderId(int orderId, int itemId);
+bool UpdateOrderItemsHasPaidFlag(bool hasPaid);
+
+*/
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12341234"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  con.query("USE MODERN_WAITER_DB", function(err,result,fields){
+    if (err) throw err;
+    });
+  main();
+
+
+function main(){
+    GetRestaurantById(1);
+}
+
+
+
+
