df3b8bc63fc71905dce44a9db843fd151bac3054
Tawsif <tawsifhasan1998@gmail.com>
2020-10-25 11:49:49 -0700

    Update route names

--- BackEnd/backend_server.js
+++ BackEnd/backend_server.js
@@ -51,7 +51,7 @@ app.get("/items/:id", (req,res) =>{
 });
 
 //Get item options by item id
-app.get("/item_options/:id", (req,res) => {
+app.get("/item-options/:id", (req,res) => {
     let id = req.params.id;
     let sql_query = mysql.format("SELECT * FROM items_options WHERE items_id = ?", [id]);
     con.query(sql_query, function(err,result,fields){
@@ -113,7 +113,7 @@ app.post("/order", (req,res) =>{
 })
 
 //Update order amount with a given amount
-app.post("/add/amount/to/order", (req,res) => {
+app.put("/order/amount", (req,res) => {
     let id = req.body.id;
     let amount = req.body.amount;
     let sql_query_get_oldamount = mysql. format("SELECT amount FROM orders WHERE id = ?", [id]);
@@ -138,9 +138,9 @@ app.post("/add/amount/to/order", (req,res) => {
 })
 
 //Get order by userID
-app.get("/order/user/id/:users_id/:isActive", (req,res) =>{
+app.get("/order/user/:users_id", (req,res) =>{
     let users_id = req.params.users_id;
-    let isActive = req.params.isActive;
+    let isActive = req.query.isActive;
     let sql_query = mysql.format("SELECT * FROM orders WHERE users_id = ? && is_active_session = ? ", [users_id, isActive]);
     con.query(sql_query, function(err,result,fields){
         if (err) {
@@ -151,9 +151,9 @@ app.get("/order/user/id/:users_id/:isActive", (req,res) =>{
 })
 
 //Get order by tablesID
-app.get("/order/table/id/:tables_id/:isActive", (req,res) =>{
+app.get("/order/table/:tables_id", (req,res) =>{
     let tables_id = req.params.tables_id;
-    let isActive = req.params.isActive;
+    let isActive = req.query.isActive;
     let sql_query = mysql.format("SELECT * FROM orders WHERE tables_id = ? && is_active_session = ? ", [tables_id, isActive]);
     con.query(sql_query, function(err,result,fields){
         if (err) {
@@ -165,7 +165,7 @@ app.get("/order/table/id/:tables_id/:isActive", (req,res) =>{
 
 
 // If the session is complete, set the active session flag to false
-app.post("/order/is/active/session", (req,res) => {
+app.put("/order/session", (req,res) => {
     let orderId = req.body.orderId;
     let isActive = req.body.isActive;
     let sql_query = mysql.format("UPDATE orders SET is_active_session = ? WHERE id = ?", [isActive, orderId]);
@@ -180,7 +180,7 @@ app.post("/order/is/active/session", (req,res) => {
 
 
 //If all the ordered items have been paid for, mark this as has paid 
-app.post("/order/has/paid", (req,res) => {
+app.put("/order/paid", (req,res) => {
     let orderId = req.body.orderId;
     let hasPaid = req.body.hasPaid;
     let sql_query = mysql.format("UPDATE orders SET has_paid = ? WHERE id = ?", [hasPaid,orderId]);
@@ -196,7 +196,7 @@ app.post("/order/has/paid", (req,res) => {
 })
 
 //Gets ordered items via orderid
-app.get("/ordered/items/:orderId", (req,res) => {
+app.get("/ordered-items/:orderId", (req,res) => {
     let orderId = req.params.orderId;
     let sql_query = mysql.format("SELECT * FROM ordered_items WHERE orders_id = ?", [ orderId]);
     con.query(sql_query, function(err,result,fields){
@@ -208,7 +208,7 @@ app.get("/ordered/items/:orderId", (req,res) => {
 })
 
 // adds a new item to the ordered items table, used when you add an item to your meal later on during your time at the restaurant
-app.post("/add/item/to/order", (req,res) => {
+app.post("/ordered-items/:orderId", (req,res) => {
     let orderId = req.body.orderId;
     let itemId = req.body.itemId;
     let sql_query = mysql.format("INSERT INTO ordered_items (orders_id, items_id, has_paid, is_selected) VALUES(?,?, 0,0) ", [orderId,itemId]);
@@ -222,7 +222,7 @@ app.post("/add/item/to/order", (req,res) => {
 
 // if you pay for the specific item, mark it as has paid
 //TODO: should we add userıd so we can send push notification saying x user paid y item
-app.post("/mark/item/has/paid", (req,res) => {
+app.put("/ordered-items/paid", (req,res) => {
     let orderId = req.body.orderId;
     let itemId = req.body.itemId;
     let hasPaid = req.body.hasPaid;
