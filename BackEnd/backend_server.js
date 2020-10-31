295e6955a7a734421db06006e29d76c3734002b7
Tawsif <tawsifhasan1998@gmail.com>
2020-10-31 01:31:39 -0700

    Removed redundant code

--- BackEnd/backend_server.js
+++ BackEnd/backend_server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mysql = require('mysql');
-// const { subscribe, messageAccountisClosed } = require("./push_notification.js");
-const app = express();
-const push_notification = require("./push_notification.js");
+const express = require("express")
+const mysql = require('mysql')
+// const { subscribe, messageAccountisClosed } = require("./push_notification.js")
+const app = express()
+const push_notification = require("./push_notification.js")
 
 var con = mysql.createConnection({
     host: "localhost", 
@@ -11,7 +11,7 @@ var con = mysql.createConnection({
     database: "MODERN_WAITER_DB", 
     port: 3306, 
     ssl:true
-});
+})
 
 // Reference to the API routes
 const items = require('./routes/items.js')(app, con)
@@ -24,20 +24,20 @@ const restaurants = require('./routes/restaurants.js')(app, con)
 const tables = require('./routes/tables.js')(app, con)
 const users = require('./routes/users.js')(app, con)
 
-app.use(express.json());
+app.use(express.json())
 
 con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+    if (err) throw err
+    console.log("Connected!")
     var server = app.listen(3000,function(){
-        var port = server.address().port;
-        console.log("Server started listening at %s", port);
-    });
-});
+        var port = server.address().port
+        console.log("Server started listening at %s", port)
+    })
+})
 
 // function pushNotificationsDemo(){
 //     subscribe("dti7Svc4SC6utD7GPz9ZXy:APA91bEVZQYS-PJ1OYgYqbOElQkM_BTI7Si_S3eLXOpO-oIpM155VGAJzl-FJHYFUNMMYdfg3cOvWM6bX5X-6m6k7H6QQCdZA96qEZt3lwRpE68iOmb7uVx8hfbx5SZUuy8MnnTdGArg","1")
-//     messageAccountisClosed(1);
+//     messageAccountisClosed(1)
 // }
 
 /**
@@ -45,18 +45,19 @@ con.connect(function(err) {
  * push notification service.
  */
 app.post("/registrationToken", (req,res) => {
-    let registrationToken = req.body.registrationToken;
- console.log(registrationToken);
-	let orderId = "1";
-    res.send(push_notification.subscribe(registrationToken, orderId));
+    console.log("/registrationToken")
+    let registrationToken = req.body.registrationToken
+    console.log(registrationToken)
+	let orderId = "1"
+    res.send(push_notification.subscribe(registrationToken, orderId))
 })
 
 //Informs server that the current cart is checked out
 app.get("/checkout", (req,res) =>{
-    console.log("Checkout received");
-    push_notification.push_notification_order_received("1");
-    res.send("Success");
-});
+    console.log("Checkout received")
+    push_notification.push_notification_order_received("1")
+    res.send("Success")
+})
 
 /* HTTP GET request for health check */
 app.get('/_health', (req, res) => {
