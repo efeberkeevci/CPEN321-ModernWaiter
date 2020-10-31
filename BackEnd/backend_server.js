af16ed0ee54d39b475df4e4d2a8c319ffcd9b593
Tawsif <tawsifhasan1998@gmail.com>
2020-10-30 20:35:38 -0700

    Updated items.js

--- BackEnd/backend_server.js
+++ BackEnd/backend_server.js
@@ -57,25 +57,6 @@ app.get("/restaurant/:id", (req,res) => {
 });
 
 /**
- * HTTP GET request to acquire items of a
- * restaurant representing its menu. It
- * returns a list of all the items belonging
- * to the restaurant with a status code of
- * 200 if successful.
- */
-app.get("/items/:id", (req,res) =>{
-    console.log("/items/{{id}}");
-    let id = req.params.id;
-    let sql_query = mysql.format("SELECT * FROM items WHERE restaurant_id = ?", [id]);
-    con.query(sql_query, function(err,result,fields){
-        if (err) {
-            res.send(err);
-        }
-        res.send(result);
-    });
-});
-
-/**
  * HTTP GET request to list option ids for
  * every item. This can be used to 
  * identify every additional option per item,
--- BackEnd/routes/items.js
+++ BackEnd/routes/items.js
@@ -1,9 +1,38 @@
 const express = require('express')
-const router = express.Router
+const mysql = require('mysql');
+const router = express.Router()
+
+var con = mysql.createConnection({
+    host: "localhost", 
+    user: "admin", 
+    password: "modernwaitercpen321!", 
+    database: "MODERN_WAITER_DB", 
+    port: 3306, 
+    ssl:true
+});
 
 router.get('/test', (req, res) => {
     console.log("Test here")
     res.end()
 })
+
+/**
+ * HTTP GET request to acquire items of a
+ * restaurant representing its menu. It
+ * returns a list of all the items belonging
+ * to the restaurant with a status code of
+ * 200 if successful.
+ */
+app.get("/items/:id", (req,res) =>{
+    console.log("/items/{{id}}");
+    let id = req.params.id;
+    let sql_query = mysql.format("SELECT * FROM items WHERE restaurant_id = ?", [id]);
+    con.query(sql_query, function(err,result,fields){
+        if (err) {
+            res.send(err);
+        }
+        res.send(result);
+    });
+});
 
 module.exports = router
