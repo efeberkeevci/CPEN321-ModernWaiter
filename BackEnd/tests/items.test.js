962b1cf50f70a591437e94e64fc5a44a9af816ec
Tawsif <tawsifhasan1998@gmail.com>
2020-11-01 14:04:19 -0800

    Removed async

--- BackEnd/tests/items.test.js
+++ BackEnd/tests/items.test.js
@@ -4,7 +4,7 @@ const app = require('../backend_server')
 jest.mock('../mocks/items')
 
 describe("GET /items/{{restaurantId}}", () => {
-    test("It should respond with an array of items", async () => {
+    test("It should respond with an array of items", () => {
       const response = await request(app).get("/items/1");
       expect(response.body).hasAssertions();
       expect(response.statusCode).toBe(200);
--- BackEnd/tests/orders.test.js
+++ BackEnd/tests/orders.test.js
@@ -1,7 +1,7 @@
 const orders = require('../routes/orders.js')
 
 describe('checkOrder', () => {
-  test('should 200 with username from session if session data is set', async () => {
+  test('should 200 with username from session if session data is set', () => {
     const req = {body: { userId : "1", tableId : "1", restaurantId : "1", amount : "55.43", hasPaid : "0", isActive : "1"}}
     //const res = mockResponse();
     orders.createOrder(req, res);
