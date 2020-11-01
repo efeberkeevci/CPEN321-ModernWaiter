2e00efd52b741ec6feffd45a4bf48a727d1ea686
Tawsif <tawsifhasan1998@gmail.com>
2020-11-01 11:18:33 -0800

    Test bad request

--- BackEnd/tests/orders.test.js
+++ BackEnd/tests/orders.test.js
@@ -3,7 +3,7 @@ const orders = require('../routes/orders.js')
 describe('checkOrder', () => {
   test('should 200 with username from session if session data is set', async () => {
     const req = {body: { userId : "1", tableId : "1", restaurantId : "1", amount : "55.43", hasPaid : "0", isActive : "1"}}
-    const res = mockResponse();
+    //const res = mockResponse();
     orders.createOrder(req, res);
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toEqual("");
