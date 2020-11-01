c3e5cccfe76b4ac69bd0de3ab73f3fe77c3d1a5f
VincentSastra <vincent.sastra@gmail.com>
2020-11-01 14:02:04 -0800

    Reset test back to normal

--- FrontEnd/app/src/test/java/com/cpen321/modernwaiter/ExampleUnitTest.java
+++ FrontEnd/app/src/test/java/com/cpen321/modernwaiter/ExampleUnitTest.java
@@ -1,34 +1,17 @@
 package com.cpen321.modernwaiter;
 
-
-import androidx.fragment.app.testing.FragmentScenario;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.filters.LargeTest;
-
-import com.cpen321.modernwaiter.ui.menu.MenuFragment;
-
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(JUnit4.class)
-@LargeTest
 public class ExampleUnitTest {
-
-    @Before
-    public void init() {
-    }
-
     @Test
-    public void mockActivityExist() {
-        FragmentScenario.launch(MenuFragment.class);
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
 }
