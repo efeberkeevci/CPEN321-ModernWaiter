cc9d805077fe1cf3bf3fb2cc8c8a39cee5278335
Ubuntu <modernwaiter@modernwaiter.k5rhlfxwj5surlpybjnkqqekwd.bx.internal.cloudapp.net>
2020-11-01 22:19:25 +0000

    Updated placeholder test

--- BackEnd/tests/orders.test.js
+++ BackEnd/tests/orders.test.js
@@ -5,11 +5,11 @@ describe('checkOrder', () => {
     const req = mockRequest({ userId : "1", tableId : "1", restaurantId : "1", amount : "55.43", hasPaid : "0", isActive : "1"})
     const res = mockResponse();
 
-    orders.createOrder(req, res);
+    //orders.createOrder(req, res);
     // expect(res.status).toEqual(200);
     // expect(res.json).toEqual("");
     expect(1).toEqual(1); //placeholder
-    done()
+    //done()
   });
 });
 
