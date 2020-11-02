71684380b768868c8006aa1c3fc173f6e6531657
Tawsif <tawsifhasan1998@gmail.com>
2020-11-02 10:54:05 -0800

    Updated test

--- BackEnd/tests/recommendation.test.js
+++ BackEnd/tests/recommendation.test.js
@@ -3,17 +3,25 @@ const supertest = require('supertest')
 const request = supertest(app)
 const recommendation_logic = require('../recommendation_logic')
 
-describe('getItemRecommendation()', async done => {
-    test('Should 200', () => {
-      // Arrange
-      //const mock = jest.fn().mockReturnValue({itemId : 1})
-      //recommendation_logic.getRecommendation = mock
+// describe('getItemRecommendation()', async done => {
+//     test('Should 200', () => {
+//       // Arrange
+//       //const mock = jest.fn().mockReturnValue({itemId : 1})
+//       //recommendation_logic.getRecommendation = mock
 
-      // Act
-      const response = request.get('/recommendation/1/1')
+//       // Act
+//       const response = request.get('/recommendation/1/1')
 
-      // Assert
-      expect(response.status).toBe(200)
-      done()
-    });
-  });
+//       // Assert
+//       expect(response.status).toBe(200)
+//       done()
+//     });
+//   });
+
+  it('gets the test endpoint', async done => {
+    const response = await request.get('/recommendation/1/1')
+  
+    expect(response.status).toBe(200)
+    //expect(response.body.message).toBe('pass!')
+    done()
+  })
