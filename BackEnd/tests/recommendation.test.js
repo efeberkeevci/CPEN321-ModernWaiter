9b07a053193da976d3bfdb5482be0fb89a211a5c
Ubuntu <modernwaiter@modernwaiter.2ic52r2wudlexmq15gr25d2mkf.bx.internal.cloudapp.net>
2020-11-02 18:48:12 +0000

    Changes to test

--- BackEnd/tests/recommendation.test.js
+++ BackEnd/tests/recommendation.test.js
@@ -5,15 +5,12 @@ const recommendation_logic = require('../recommendation_logic')
 
 describe('getItemRecommendation()', () => {
     test('Should 200', () => {
-      const req = mockRequest({ userId : "1", tableId : "1", restaurantId : "1", amount : "55.43", hasPaid : "0", isActive : "1"})
-      const res = mockResponse();
-
       // Arrange
       const mock = jest.fn().mockReturnValue({itemId : 1})
       recommendation_logic.getRecommendation = mock
 
       // Act
-      const response = await request.get('/recommendation/1/1')
+      const response = request.get('/recommendation/1/1')
 
       // Assert
       expect(response.status).toBe(200)
