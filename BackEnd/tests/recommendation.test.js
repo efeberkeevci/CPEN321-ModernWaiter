3645df40c44530bde0ad11055274c49c59d138b4
Tawsif <tawsifhasan1998@gmail.com>
2020-11-02 12:10:45 -0800

    Updated cases

--- BackEnd/tests/recommendation.test.js
+++ BackEnd/tests/recommendation.test.js
@@ -4,17 +4,75 @@ const request = supertest(app)
 const recommendation_logic = require('../recommendation_logic')
 
 describe('Test getItemRecommendation() with mock recommendation logic', () => {
-    it('Gets the recommended item with itemId 1', async done => {
+    it('Gets the recommended item with valid user and restaurant', async done => {
         // Arrange
-        const mock = jest.fn().mockReturnValue(1)
+        const userId = 1
+        const restaurantId = 1
+        const url = `/recommendation/${userId}/${restaurantId}`
+        const expectedItemId = 5
+        const mock = jest.fn().mockReturnValue(expectedItemId)
         recommendation_logic.getRecommendation = mock
 
         // Act
-        const response = await request.get('/recommendation/1/1')
+        const response = await request.get(url)
 
         // Assert
         expect(response.status).toBe(200)
-        expect(response.body).toStrictEqual({itemId : 1})
+        expect(response.body).toStrictEqual({itemId : expectedItemId})
+        done()
+      })
+
+      it('Gets the recommended item with another valid user and restaurant', async done => {
+        // Arrange
+        const userId = 2
+        const restaurantId = 1
+        const url = `/recommendation/${userId}/${restaurantId}`
+        const expectedItemId = 3
+        const mock = jest.fn().mockReturnValue(expectedItemId)
+        recommendation_logic.getRecommendation = mock
+
+        // Act
+        const response = await request.get(url)
+
+        // Assert
+        expect(response.status).toBe(200)
+        expect(response.body).toStrictEqual({itemId : expectedItemId})
+        done()
+      })
+
+      it('Gets the recommended item with valid user and invalid restaurant', async done => {
+        // Arrange
+        const userId = 1
+        const restaurantId = 5
+        const url = `/recommendation/${userId}/${restaurantId}`
+        const expectedItemId = 5
+        const mock = jest.fn().mockReturnValue(expectedItemId)
+        recommendation_logic.getRecommendation = mock
+
+        // Act
+        const response = await request.get(url)
+
+        // Assert
+        expect(response.status).toBe(200)
+        expect(response.body).toStrictEqual({})
+        done()
+      })
+
+      it('Gets the recommended item with invalid user and valid restaurant', async done => {
+        // Arrange
+        const userId = 10
+        const restaurantId = 1
+        const url = `/recommendation/${userId}/${restaurantId}`
+        const expectedItemId = 5
+        const mock = jest.fn().mockReturnValue(expectedItemId)
+        recommendation_logic.getRecommendation = mock
+
+        // Act
+        const response = await request.get(url)
+
+        // Assert
+        expect(response.status).toBe(200)
+        expect(response.body).toStrictEqual({})
         done()
       })
   })
