71f64ba77ffb83428cf744f864b2b071063f92b4
Efe Evci <efeberkeevci@gmail.com>
2020-11-17 01:47:44 +0300

    Created the test file for ordered_items module
    Formed the skeleton of the testing code

---
+++ BackEnd/tests/ordered_items.test.js
@@ -0,0 +1,20 @@
+const app = require('../backend_server')
+const supertest = require('supertest')
+const request = supertest(app)
+
+//Tests for GET /ordered-items/{{orderId}}
+describe("Test getOrderedItems()", ()=>{
+
+})
+
+describe("Test addOrderedItems()",()=>{
+
+})
+
+describe("Test updateSelectedStatus()",() =>[
+
+])
+
+describe("Test updateOrderedItemPaidStatus()", () =>{
+
+})
