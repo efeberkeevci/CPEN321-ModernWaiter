52e284887e2d9915f01ff83b3eaaf3bb8c71d6ad
Efe Evci <efeberkeevci@gmail.com>
2020-11-17 02:35:26 +0300

    Wrote tests for addOrderedItems

--- BackEnd/tests/ordered_items.test.js
+++ BackEnd/tests/ordered_items.test.js
@@ -5,7 +5,6 @@ const request = supertest(app)
 //Tests for GET /ordered-items/{{orderId}}
 describe("Test getOrderedItems()", ()=>{
 
-    //Testing Invalid orderId type
     test("Testing Invalid orderId type", async done=>{
 
         //Arrange
@@ -20,7 +19,6 @@ describe("Test getOrderedItems()", ()=>{
         done()
     })
     
-    //Testing valid orderId type
     test("Testing valid orderId type", async done=>{
 
         //Arrange
@@ -33,19 +31,60 @@ describe("Test getOrderedItems()", ()=>{
         //Assert
         //TODO: What to do for the case query returns 400 although valid
         expect(res.statusCode).toEqual(200)
-        expect(typeof response.body[0].id).toBe('number')
-        expect(typeof response.body[0].orders_id).toBe('number')
-        expect(typeof response.body[0].items_id).toBe('number')
-        expect(typeof response.body[0].has_paid).toBe('number')
-        expect(typeof response.body[0].is_selected).toBe('number')
-        expect(typeof response.body[0].users_id).toBe('number')
+        expect(typeof res.body[0].id).toBe('number')
+        expect(typeof res.body[0].orders_id).toBe('number')
+        expect(typeof res.body[0].items_id).toBe('number')
+        expect(typeof res.body[0].has_paid).toBe('number')
+        expect(typeof res.body[0].is_selected).toBe('number')
+        expect(typeof res.body[0].users_id).toBe('number')
         done()
     })
 
 })
 
 describe("Test addOrderedItems()",()=>{
+    test("Test Invalid request body", async done=>{
 
+        //Arrange
+        let req_body ={ 
+            "body":
+                [
+                    {"orderId" : "invalid orderId type",
+                     "itemId"  : "1"
+                    },
+                    {"orderId" : "2",
+                     "itemId"  : "invalid type"
+                    },
+                ]
+        }
+        let path = "/ordered-items/selected"
+
+        //Act
+        const res = await request.put(path).send(req_body)
+
+        //Assert
+        expect(res.statusCode).toEqual(400)
+    })
+
+    test("Test Valid request body", async done=>{
+
+        //Arrange
+        let req_body ={ 
+            "body":
+                [
+                    {"orderId" : "1",
+                     "itemId"  : "1"
+                    }
+                ]
+        }
+        let path = "/ordered-items/selected"
+
+        //Act
+        const res = await request.put(path).send(req_body)
+
+        //Assert
+        expect(res.statusCode).toEqual(200)
+    })
 })
 
 describe("Test updateSelectedStatus()",() =>{
