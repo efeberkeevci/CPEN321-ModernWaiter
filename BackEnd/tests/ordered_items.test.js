e699fcca3846849de8a4013ac204bf3d5b5686f6
Efe Evci <efeberkeevci@gmail.com>
2020-11-17 02:03:14 +0300

    Implemented test for the getOrderedıtems function

--- BackEnd/tests/ordered_items.test.js
+++ BackEnd/tests/ordered_items.test.js
@@ -5,6 +5,35 @@ const request = supertest(app)
 //Tests for GET /ordered-items/{{orderId}}
 describe("Test getOrderedItems()", ()=>{
 
+    //Testing Invalid orderId type
+    test("Testing Invalid orderId type", async done=>{
+
+        //Arrange
+        let param = "Invalid orderId of wrong data type"
+        let path = "/ordered-items/"
+        
+        //Act
+        const res = await request.get(path+param)
+
+        //Assert
+        expect(res.statusCode).toEqual(400)
+        done()
+    })
+    
+    //Testing valid orderId type
+    test("Testing valid orderId type", async done=>{
+
+        //Arrange
+        let param = "1"
+        let path = "/ordered-items/"
+        
+        //Act
+        const res = await request.get(path+param)
+
+        //Assert
+        //TODO: What to do for the case query returns 400 although valid
+        expect(res.statusCode).toEqual(200)
+        done()
 })
 
 describe("Test addOrderedItems()",()=>{
