541734806eaccac9a9e7413d8a46ce472b71b83d
dahiya18 <harmeetadahiya@gmail.com>
2020-11-17 00:09:18 -0800

    added tests in IntegratedTesting

--- FrontEnd/app/src/androidTest/java/com/cpen321/modernwaiter/IntegratedTesting.java
+++ FrontEnd/app/src/androidTest/java/com/cpen321/modernwaiter/IntegratedTesting.java
@@ -1,12 +1,21 @@
 package com.cpen321.modernwaiter;
 
+import android.support.test.InstrumentationRegistry;
+
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject2;
+import androidx.test.uiautomator.Until;
 
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
 import com.cpen321.modernwaiter.customer.application.ApiUtil;
 import com.cpen321.modernwaiter.customer.application.CustomerActivity;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,8 +28,14 @@ import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.android.volley.Request.Method.GET;
+import static org.junit.Assert.assertEquals;
 
 public class IntegratedTesting {
+
+    private final static String NOTIFICATION_TITLE = "Order Received!";
+    private final static String NOTIFICATION_TEXT = "Order Received";
+    private final static int TIMEOUT = 3000;
 
     @Before
     public void changeUserAndTableId() throws InterruptedException {
@@ -31,6 +46,18 @@ public class IntegratedTesting {
     }
 
     /**
+     * Check if the server is running
+     */
+    @Test public void checkServerConnection(){
+        StringRequest stringRequest = new StringRequest(GET, ApiUtil.health, response -> {
+            if(response == null) Assert.fail("Received null response from backend health checkup");
+            else Assert.assertTrue( true);
+        }, error -> {
+            Assert.fail("Could not connect to the server" + error.toString());
+        });
+    }
+
+    /**
      * add item to cart
      * view cart
      * check if the item was added to the cart
@@ -38,76 +65,83 @@ public class IntegratedTesting {
      */
     @Test
     public void addMenuItemToOrder(){
-        //view menu as default (start_destination)
-        onView(withId(R.id.fragment_menu))
-                .check(matches(isDisplayed()));
+        try {
+            //view menu as default (start_destination)
+            onView(withId(R.id.fragment_menu))
+                    .check(matches(isDisplayed()));
 
-        //check that the menu items list is displayed
-        onView(withId(R.id.menu_recycler))
-                .check(matches(isDisplayed()));
+            //check that the menu items list is displayed
+            onView(withId(R.id.menu_recycler))
+                    .check(matches(isDisplayed()));
 
-        //make sure that recycler view is displayed
-        onView(withId(R.id.menu_recycler))
-                .check(matches(isDisplayed()));
+            //make sure that recycler view is displayed
+            onView(withId(R.id.menu_recycler))
+                    .check(matches(isDisplayed()));
 
-        //click on a menu item
-        onView(withId(R.id.menu_recycler))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+            //click on a menu item
+            onView(withId(R.id.menu_recycler))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
-        //check if the menu item detail view is displayed
-        onView(withId(R.id.fragment_menu))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.topCardView))
-                .check(matches(isDisplayed()));
+            //check if the menu item detail view is displayed
+            onView(withId(R.id.fragment_menu))
+                    .check(matches(isDisplayed()));
+            onView(withId(R.id.topCardView))
+                    .check(matches(isDisplayed()));
 
-        String addedItem = "Spicy Ahi Roll";
-        /**
-         * Now add this item to the cart
-         */
-        onView(withId(R.id.incrementButton))
-                .perform(click());
+            String addedItem = "Spicy Ahi Roll";
+            /**
+             * Now add this item to the cart
+             */
+            onView(withId(R.id.incrementButton))
+                    .perform(click());
 
-        //now go back to menu
-        onView(withId(R.id.exitButton))
-                .perform(click());
+            //now go back to menu
+            onView(withId(R.id.exitButton))
+                    .perform(click());
 
-        //check that in menu fragment
-        onView(withId(R.id.fragment_menu))
-                .check(matches(isDisplayed()));
+            //check that in menu fragment
+            onView(withId(R.id.fragment_menu))
+                    .check(matches(isDisplayed()));
 
-        //click view cart
-        onView(withId(R.id.viewCartButton))
-                .perform(click());
+            //click view cart
+            onView(withId(R.id.viewCartButton))
+                    .perform(click());
 
-        //check that cart is displayed
-        onView(withId(R.id.fragment_order))
-                .check(matches(isDisplayed()));
+            //check that cart is displayed
+            onView(withId(R.id.fragment_order))
+                    .check(matches(isDisplayed()));
 
-        //check that order_recycler is displayed
-        onView(withId(R.id.order_recycler))
-                .check(matches(isDisplayed()));
+            //check that order_recycler is displayed
+            onView(withId(R.id.order_recycler))
+                    .check(matches(isDisplayed()));
 
-        //check that added item is displayed
-        // Attempt to scroll to an item that contains the special text.
-        onView(withId(R.id.order_recycler))
-                // scrollTo will fail the test if no item matches.
-                .perform(RecyclerViewActions.scrollTo(
-                        hasDescendant(withText(addedItem))
-                ));
+            //check that added item is displayed
+            // Attempt to scroll to an item that contains the special text.
+            onView(withId(R.id.order_recycler))
+                    // scrollTo will fail the test if no item matches.
+                    .perform(RecyclerViewActions.scrollTo(
+                            hasDescendant(withText(addedItem))
+                    ));
 
-        //////////cleanup///////////////////
+            //////////cleanup///////////////////
 
-        //now click on menu in bottom navigation bar
-        onView(withId(R.id.navigation_menu))
-                .perform(click());
+            //now click on menu in bottom navigation bar
+            onView(withId(R.id.navigation_menu))
+                    .perform(click());
 
-        //click on a menu item
-        onView(withId(R.id.menu_recycler))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+            //click on a menu item
+            onView(withId(R.id.menu_recycler))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
-        //remove the item
-        onView(withId(R.id.decrementButton))
-                .perform(click());
+            //remove the item
+            onView(withId(R.id.decrementButton))
+                    .perform(click());
+
+            Assert.assertTrue( true);
+        }
+        catch(Exception e){
+            Assert.fail("************Could not add menu item to order*************\n" + e.getMessage());
+        }
 
     }
 
@@ -122,43 +156,60 @@ public class IntegratedTesting {
         //by default on menu
 
         //click on a menu item
-        onView(withId(R.id.menu_recycler))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        try {
+            onView(withId(R.id.menu_recycler))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
-        String addedItem = "Spicy Ahi Roll";
+            String addedItem = "Spicy Ahi Roll";
 
-        //add item to cart
-        onView(withId(R.id.incrementButton))
-                .perform(click());
+            //add item to cart
+            onView(withId(R.id.incrementButton))
+                    .perform(click());
 
-        //now go back to menu
-        onView(withId(R.id.exitButton))
-                .perform(click());
+            //now go back to menu
+            onView(withId(R.id.exitButton))
+                    .perform(click());
+            try{
+                onView(withId(R.id.viewCartButton))
+                        .check(matches(isDisplayed()));
+            } catch(Exception e){
+                Assert.fail("View Cart Button is not displayed");
+                return;
+            }
+            try {
+                //click view cart
+                onView(withId(R.id.viewCartButton))
+                        .perform(click());
+            }
+            catch(Exception e){
+                Assert.fail("Click ViewCartButton is not working");
+                return;
+            }
+            //click on checkout
+            onView(withId(R.id.checkoutButton))
+                    .perform(click());
 
-        //click view cart
-        onView(withId(R.id.viewCartButton))
-                .perform(click());
+            //click on view bill
+            onView(withId(R.id.startBillButton))
+                    .perform(click());
 
-        //click on checkout
-        onView(withId(R.id.checkoutButton))
-                .perform(click());
+            //check that its displaying the bill
+            onView(withId(R.id.fragment_bill))
+                    .check(matches(isDisplayed()));
 
-        //click on view bill
-        onView(withId(R.id.startBillButton))
-                .perform(click());
-
-        //check that its displaying the bill
-        onView(withId(R.id.fragment_bill))
-                .check(matches(isDisplayed()));
-
-        //check that item is added to the bill
-        onView(withId(R.id.bill_recycler))
+            //check that item is added to the bill
+            onView(withId(R.id.bill_recycler))
                     // scrollTo will fail the test if no item matches.
                     .perform(RecyclerViewActions.scrollTo(
                             hasDescendant((withText(addedItem)))
                     ));
 
+            Assert.assertTrue( true);
 
+        } catch(Exception e){
+            Assert.fail("***********Could not add item to the table order*************\n" + e.getMessage());
+        }
+        payForAll();
     }
 
     /**TODO: write this test
@@ -166,32 +217,52 @@ public class IntegratedTesting {
      */
     @Test
     public void checkPushNotification(){
-        /*
+
         //by default on menu
+        try {
+            UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+            try {
+                //click on a menu item
+                onView(withId(R.id.menu_recycler))
+                        .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
-        //click on a menu item
-        onView(withId(R.id.menu_recycler))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+                String addedItem = "Dummy Roll";
 
-        String addedItem = "Dummy Roll";
+                //add item to cart
+                onView(withId(R.id.incrementButton))
+                        .perform(click());
 
-        //add item to cart
-        onView(withId(R.id.incrementButton))
-                .perform(click());
+                //now go back to menu
+                onView(withId(R.id.exitButton))
+                        .perform(click());
 
-        //now go back to menu
-        onView(withId(R.id.exitButton))
-                .perform(click());
+                //click view cart
+                onView(withId(R.id.viewCartButton))
+                        .perform(click());
 
-        //click view cart
-        onView(withId(R.id.viewCartButton))
-                .perform(click());
-
-        //click on checkout
-        onView(withId(R.id.checkoutButton))
-                .perform(click());
-        */
-        //TODO: not finished
+                //click on checkout
+                onView(withId(R.id.checkoutButton))
+                        .perform(click());
+            }
+            catch(Exception e){
+                Assert.fail("Something wrong with display");
+            }
+            try {
+                device.openNotification();
+                device.wait(Until.hasObject(By.text(NOTIFICATION_TITLE)), TIMEOUT);
+                UiObject2 title = device.findObject(By.text(NOTIFICATION_TITLE));
+                UiObject2 text = device.findObject(By.text(NOTIFICATION_TEXT));
+                assertEquals(NOTIFICATION_TITLE, title.getText());
+                assertEquals(NOTIFICATION_TEXT, text.getText());
+            }
+            catch(Exception e){
+                Assert.fail("Something wrong with device");
+            }
+        }
+        catch(Exception e){
+            Assert.fail("************Did not receive any notification regarding order********\n" +e.getMessage());
+        }
+        payForAll();
     }
 
     /**
@@ -262,6 +333,9 @@ public class IntegratedTesting {
                 .check(matches(withText(R.string.thank_you_post_payment)));
     }
 
+    /**
+     * Pay for all
+     */
 
 
 }
